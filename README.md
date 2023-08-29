ab09ed1d7e1bce803e25d1df282a9c02f41e1664
Suraj Shrestha <syuraj@gmail.com>
2023-08-28 22:30:51 -0400

    hello backtrader'

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Backtrader hello
+
+### bt_hello
+- Quickstart from https://www.backtrader.com/docu/quickstart/quickstart/
+
---
+++ bt_hello.py
@@ -0,0 +1,81 @@
+# %% Import libraries
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+import backtrader as bt
+
+
+class TestStrategy(bt.Strategy):
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+    def next(self):
+        if self.order:
+            return
+
+        if not self.position:
+            if self.dataclose[0] < self.dataclose[-1]:
+                    if self.dataclose[-1] < self.dataclose[-2]:
+                        self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                        self.order = self.buy()
+        else:
+            # Already in the market ... we might sell
+            if len(self) >= (self.bar_executed + 5):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+            elif order.issell():
+                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+
+    cerebro.addstrategy(TestStrategy)
+
+    datapath = './data/orcl-1995-2014.txt'
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values after this date
+        todate=datetime.datetime(2000, 12, 31),
+        reverse=False)
+
+    cerebro.adddata(data)
+    cerebro.broker.setcash(100000.0)
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
